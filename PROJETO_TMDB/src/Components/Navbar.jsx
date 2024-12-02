a2a6e189c5b48dbb6ec1b63b5381a8ba53d12e3c
otavioogo <otavioeidi@hotmail.com>
2024-12-02 19:11:55 -0300

    Arrumando o Css da pagina principal

--- PROJETO_TMDB/src/Components/Navbar.jsx
+++ PROJETO_TMDB/src/Components/Navbar.jsx
@@ -1,7 +1,14 @@
 import {  Link } from 'react-router-dom';
 import { BiCameraMovie, BiSearchAlt2 } from 'react-icons/bi'
+import './Navbar.css'
+import { useState } from 'react';
 
 const Navbar = () => {
+
+  const [search , setSearch ] = useState("")
+
+  
+
   return (
     <div className='App'> 
     <nav id="navbar">
@@ -9,7 +16,8 @@ const Navbar = () => {
        <Link to='/'><BiCameraMovie /> Movies Lib </Link>
      </h2>
        <form>
-        <input type="text" placeholder="Procure um filmes" />
+        <input type="text" placeholder="Procure um filmes" onChange={(e) => setSearch(e.target.value)} 
+        value={search}/>
         <button type="submit">
             <BiSearchAlt2 />
         </button>
--- PROJETO_TMDB/src/pages/Home.jsx
+++ PROJETO_TMDB/src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import { useState , useEffect } from "react"
 import MovieCard from "../Components/MovieCard"
 
+import "./MovieGrid.css"
+
 const moviesURL = import.meta.env.VITE_API
 const apiKey = import.meta.env.VITE_API_KEY
 
@@ -28,7 +30,7 @@ const Home = () => {
         <div className="movies-container">
           {topMovies.length === 0 && <p>Carregando...</p>}
           {topMovies.length > 0 && 
-            topMovies.map((movie) => < MovieCard movie={movie} /> )
+            topMovies.map((movie) => < MovieCard key={movie.id} movie={movie} /> )
         }
       </div>
     </div>
